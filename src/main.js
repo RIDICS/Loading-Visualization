f17ff975f7105bfb57a85f4c755dd91087a7b62d
Jakub Rada <jacub.rada@gmail.com>
2019-08-22 11:04:21 +0200

    united css and js files into one

--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
-
+document.addEventListener("DOMContentLoaded", function(event) {
+    init();
+    progress.line_element = document.getElementsByClassName("definite_line")[0].firstElementChild;
+    progress.line_end_element = document.getElementsByClassName("line_end")[0].firstElementChild;
+});
+// fills all spinners with appropriate number of divs
+function init() {
+    // list of all possible objects with the number of divs that are supposed to be in them
+    let objects = {
+        "bars": 8,
+        "squares": 4,
+        "line": 1,
+        "circles": 12,
+        "borderless_line": 1,
+        "dots": 4,
+        "definite_line": 1,
+        "line_end": 1
+    };
+    // iterates through everything and adds specified number of divs
+    for(let key in objects) {
+        let objectsOfClass = document.getElementsByClassName(key);
+        for(let i = 0; i < objectsOfClass.length; i += 1) {
+            for(let n = 0; n < objects[key]; n += 1) {
+                objectsOfClass.item(i).appendChild(document.createElement("DIV"));
+            }
+        }
+    }
+}
 function transfer() {
     progress.previous_value = progress.current_value;
 }
@@ -63,8 +90,3 @@ progress.update_line = function() {
         }
     }
 };
-
-document.addEventListener("DOMContentLoaded", function(event) {
-    progress.line_element = document.getElementsByClassName("definite_line")[0].firstElementChild;
-    progress.line_end_element = document.getElementsByClassName("line_end")[0].firstElementChild;
-});
